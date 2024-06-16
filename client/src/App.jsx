2f09435d422b1a8f77528fe85b8cebd84989ed8c
Vinayak Gupta <vinayak20.in@gmail.com>
2024-06-16 10:47:55 +0530

    changes in location

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes,Route } from "react-router-dom"
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Homepage from "./pages/Homepage"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
